f8bdb525000c88d537ef46fada3d4ebef3c5376e
Branko <brankosaponjic76@gmail.com>
2020-10-06 10:37:14 +0200

    Multiple property files.

--- src/main/java/com/brankosaponjic/dependencyinjection/DependencyInjectionApplication.java
+++ src/main/java/com/brankosaponjic/dependencyinjection/DependencyInjectionApplication.java
@@ -2,6 +2,7 @@ package com.brankosaponjic.dependencyinjection;
 
 import com.brankosaponjic.dependencyinjection.controllers.MyController;
 import com.brankosaponjic.dependencyinjection.examplebeans.FakeDataSource;
+import com.brankosaponjic.dependencyinjection.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -21,6 +22,10 @@ public class DependencyInjectionApplication {
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
 		System.out.println(fakeDataSource.getUser());
+
+		FakeJmsBroker fakeJmsBroker = ctx.getBean(FakeJmsBroker.class);
+
+		System.out.println(fakeJmsBroker.getUser());
 	}
 
 }
--- src/main/java/com/brankosaponjic/dependencyinjection/config/PropertyConfig.java
+++ src/main/java/com/brankosaponjic/dependencyinjection/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package com.brankosaponjic.dependencyinjection.config;
 
 import com.brankosaponjic.dependencyinjection.examplebeans.FakeDataSource;
+import com.brankosaponjic.dependencyinjection.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -21,6 +27,22 @@ public class PropertyConfig {
     String password;
     @Value("${guru.dburl}")
     String url;
+
+    @Value("${jms.username}")
+    String jmsUsername;
+    @Value("${jms.password}")
+    String jmsPassword;
+    @Value("${jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUser(jmsUsername);
+        fakeJmsBroker.setPass(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
+    }
 
     @Bean
     public FakeDataSource fakeDataSource() {
