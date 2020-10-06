4f64dc8abed535a6921b0b7265a2d7a88aee72b8
Branko <brankosaponjic76@gmail.com>
2020-10-06 09:58:53 +0200

    Data properties.

--- src/main/java/com/brankosaponjic/dependencyinjection/DependencyInjectionApplication.java
+++ src/main/java/com/brankosaponjic/dependencyinjection/DependencyInjectionApplication.java
@@ -1,13 +1,15 @@
 package com.brankosaponjic.dependencyinjection;
 
 import com.brankosaponjic.dependencyinjection.controllers.MyController;
+import com.brankosaponjic.dependencyinjection.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"newpackage.services", "com.brankosaponjic.dependencyinjection.controllers"})
+@ComponentScan(basePackages = {"newpackage.services", "com.brankosaponjic.dependencyinjection.controllers",
+		"com.brankosaponjic.dependencyinjection.config", "com.brankosaponjic.dependencyinjection.examplebeans"})
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
@@ -16,9 +18,9 @@ public class DependencyInjectionApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String hello = myController.sayHelloToSpring();
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-		System.out.println(hello);
+		System.out.println(fakeDataSource.getUser());
 	}
 
 }
---
+++ src/main/java/com/brankosaponjic/dependencyinjection/config/PropertyConfig.java
@@ -0,0 +1,36 @@
+package com.brankosaponjic.dependencyinjection.config;
+
+import com.brankosaponjic.dependencyinjection.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+    @Value("${guru.password}")
+    String password;
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPass(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
+                = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
