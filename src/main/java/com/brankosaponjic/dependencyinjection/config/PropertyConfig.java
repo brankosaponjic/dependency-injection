90ec3b7f4b5bc676e9e465ddf6daeae39b75c2ab
Branko <brankosaponjic76@gmail.com>
2020-10-06 10:46:50 +0200

    Spring Boot application properties.

--- src/main/java/com/brankosaponjic/dependencyinjection/config/PropertyConfig.java
+++ src/main/java/com/brankosaponjic/dependencyinjection/config/PropertyConfig.java
@@ -2,25 +2,15 @@ package com.brankosaponjic.dependencyinjection.config;
 
 import com.brankosaponjic.dependencyinjection.examplebeans.FakeDataSource;
 import com.brankosaponjic.dependencyinjection.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
 
-    @Autowired
-    Environment env;
     @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
@@ -47,16 +37,10 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPass(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
-                = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
 }
